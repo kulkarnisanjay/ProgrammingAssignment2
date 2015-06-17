fd689baf12adff5deaaedcc60782133b2e3d9fe4
Sanjay Kulkarni <sanjay.kulkarni.b@gmail.com>
2015-06-17 23:20:39 +0530

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
 
-## Write a short comment describing this function
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse of the matrix
+## get the value of the inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x = matrix()) {
+  inv <- NULL
+  ## set the matrix
+  set<-function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  ## get the matrix
+  get<-function() x
+  ## function cache the inverse of the matrix
+  setInv<-function(inverse) inv <<- inverse
+  ## retrieve the inverse
+  getInv<-function() inv
+  list(set=set, get=get, setInv=setInv, getInv=getInv)
 }
 
+## The following function returns the inverse of the matrix. It first checks if
+## the inverse has already been computed. If so, it gets the result and skips the
+## computation. If not, it computes the inverse, sets the value in the cache via
+## setinverse function.
 
-## Write a short comment describing this function
+## This function assumes that the matrix is always invertible.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+  inv <- x$getInv()
+  ## if the inverse is cached, return it
+  if(!is.null(inv)) {
+    message("Getting cached inverse")
+    return(inv)
+  }
+  ## else, get the matrix
+  data<-x$get()
+  ## and calculate the inverse
+  inv<-solve(data)
+  ## cache the inverse
+  x$setInv(inv)
+  inv
 }
+
+## Test:
+## > x = rbind(c(1, -1/4), c(-1/4, 1))
+## > m = makeCacheMatrix(x)
+## > m$get()
+##       [,1]  [,2]
+## [1,]  1.00 -0.25
+## [2,] -0.25  1.00
+
+## The cache initally does not exist, calcuate inverse
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## Fetch the cached inverse for subsequent runs
+## > cacheSolve(m)
+## Getting cached inverse.
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## >
